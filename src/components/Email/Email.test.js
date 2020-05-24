b92e1f9ac705798c880f73dd13429eb1a4e3e6bc
Iana Babanu <Iana.Babanu@endava.com>
2020-05-24 22:49:07 +0300

    testing-hw

--- src/components/Email/Email.test.js
+++ src/components/Email/Email.test.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+
+import Email from './Email';
+
+const email = 'john.doe@mail.com';
 
 describe('<Email />', function () {
-  it('should renders show email button', () => {
+  it('should render show email button', () => {
+    const { getByTestId, queryByText } = render(
+      <Email value={email} />
+    );
+
+    const button = getByTestId('show-email-button');
+    const emailValue = queryByText(email);
+
+    expect(button).toHaveTextContent('show email');
+    expect(emailValue).not.toBeInTheDocument();
     /**
      * Check if the right button is displayed
      * Ensure that email text in NOT displayed
@@ -11,7 +25,20 @@ describe('<Email />', function () {
      */
 
   });
-  it('should renders value when button clicked', () => {
+  it('should render value when button clicked', () => {
+
+    const { getByTestId, queryByText } = render(
+      <Email value={email} />
+    );
+
+    const button = getByTestId('show-email-button');
+
+    fireEvent.click(button);
+    const emailValue = queryByText(email);
+
+    expect(button).not.toBeInTheDocument();
+    expect(emailValue).toBeInTheDocument();
+
     /**
      * After button was clicked
      * Check that button is NOT displayed (not exists in DOM)
--- src/components/SearchBox/SearchBox.test.js
+++ src/components/SearchBox/SearchBox.test.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { useSearchPageContext } from '../SearchPageContext';
+
+import  SearchBox  from '.';
+
+const value = 'dan';
+
+jest.mock('../SearchPageContext', () => ({
+  useSearchPageContext: jest.fn()
+}));
 
 describe('<SearchBox />', function () {
   it('should set user login on Enter', async () => {
+
+    const setUserLogin = jest.fn();
+    useSearchPageContext.mockImplementation(() => ({setUserLogin}));
+
+    const { getByTestId } = render((
+      <SearchBox
+      />
+    ));
+
+    const input = getByTestId('search-box-input');
+
+    fireEvent.change(input, {target: {value}});
+
+    expect(input).toHaveValue(value);
+
+    fireEvent.keyDown(input, {
+      key: "Enter",
+      code: "Enter",
+    });
+
+    expect(setUserLogin).toHaveBeenCalledWith(value);
+
     /**
      * Set a value to the search input
      * Expect that search input has the new value
